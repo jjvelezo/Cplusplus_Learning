1ff5ffc094d4667e72f0501d7809cec13746356e
Juan Jose Velez <83320302+jjvelezo@users.noreply.github.com>
2021-12-26 13:03:47 -0500

    Create esquema.cpp

---
+++ Codes/esquema.cpp
@@ -0,0 +1,12 @@
+/*Compilar:
+g++ -o <<programa>> <<codigoFuente.cpp>>
+g++ -o programa esquema.cpp
+*/
+
+//Librerias o paquetes
+#include <iostream>
+
+//Funciones y variables
+int main(int argc, char *argv[]) {
+  std::cout << "Hello World!";
+}
