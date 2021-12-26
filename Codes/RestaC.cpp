fd340c1dc510fea11e6aa2c4ba97ef08b35c3a93
Juan Jose Velez <83320302+jjvelezo@users.noreply.github.com>
2021-12-26 13:05:26 -0500

    Create RestaC.cpp

---
+++ Codes/RestaC.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+int numero;//Definiendo Variable
+int numero2;
+
+cout<<"Digite el numero que desea restar: ";
+cin>>numero;//Guardando Variable
+
+cout<<"Menos: ";
+cin>>numero2;
+
+
+
+
+cout<<numero - numero2<<endl;
+
+  
+  return 0;
+}
