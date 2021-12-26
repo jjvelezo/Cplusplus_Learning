0071ac801739d66de3feca49638eed95a1e98893
Juan Jose Velez <83320302+jjvelezo@users.noreply.github.com>
2021-12-26 13:04:09 -0500

    Create CicloFor.cpp

---
+++ Codes/CicloFor.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+
+    for(int i=0; i < 10; i++){
+      
+      //Notemos que escribir i-- es similar a escribir i = i - 1
+        cout << i << endl;
+    }
+    return 0;
+}
