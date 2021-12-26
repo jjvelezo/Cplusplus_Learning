ed15aad3025d257848b01b7ef232a8920d7328a4
Juan Jose Velez <83320302+jjvelezo@users.noreply.github.com>
2021-12-26 13:06:46 -0500

    Create Reto1C.cpp

---
+++ Codes/Reto1C.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+
+using namespace std;
+
+
+//---aqui usted escribe el reto (No hay arreglos)
+
+
+int main() {
+  
+   int n1, n2, suma = 0, promedio = 0;
+
+ cout<< "Digite el primer numero: ";
+ cin>>n1;
+ cout<< "digite el segundo numero: ";
+ cin>>n2;
+
+ suma = n1 + n2;
+
+ promedio = suma/2;
+ 
+  cout<< "Se registraron "<<suma<< " numeros, el promedio es: "<<promedio<<endl;
+}
