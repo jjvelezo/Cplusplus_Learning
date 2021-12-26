bad865361b2cf1c7a0bc36f67bcaa5c632af2756
Juan Jose Velez <83320302+jjvelezo@users.noreply.github.com>
2021-12-26 13:05:05 -0500

    Create Sumac.cpp

---
+++ Codes/Sumac.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+
+using namespace std;
+
+int main() {
+
+int entero = 10;
+
+float decimales = 12.82;
+
+double mayor = 16.58903;
+
+char cualquiercosa = '1';
+
+
+
+
+cout<<cualquiercosa;
+
+  return 0;
+
+}
